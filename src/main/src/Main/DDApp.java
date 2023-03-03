7293648dd3692be77e4e3a3e609acc4502f23b18
angel <angel.gomez.prol@udc.es>
2023-03-03 20:15:32 +0100

    Initial commit

---
+++ src/main/src/Main/DDApp.java
@@ -0,0 +1,40 @@
+package Main;
+
+
+public class DDApp {
+    public static void main(String[] args) {
+        Personaje p1 = new Rey("Rey Arturo");
+        Personaje p2 = new Caballero("Caballero Oscuro");
+        Personaje p3 = new Troll("Troll Gigante");
+        // Configurar ataques
+        p1.setAtaque(new AtaqueEspada());
+        p2.setAtaque(new AtaqueArco());
+        p3.setAtaque(new AtaqueCuchillo());
+
+        // Simulación de batalla
+        System.out.println("----- Inicio de la batalla -----");
+        System.out.println(p1 + " vs " + p3);
+        System.out.println(p2 + " vs " + p3);
+        System.out.println("-----------------------------");
+        while (p1.salud > 0 && p3.salud > 0) {
+            p1.ataca(p3);
+            if (p3.salud <= 0) {
+                break;
+            }
+            p3.ataca(p1);
+        }
+        System.out.println("-----------------------------");
+        while (p2.salud > 0 && p3.salud > 0) {
+            p2.ataca(p3);
+            if (p3.salud <= 0) {
+                break;
+            }
+            p3.ataca(p2);
+        }
+        System.out.println("-----------------------------");
+        System.out.println("----- Fin de la batalla -----");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
+}
